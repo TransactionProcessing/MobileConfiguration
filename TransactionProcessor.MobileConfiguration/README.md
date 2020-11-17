1833d1babb11be3973c44a23c7405c63e7f67ec8
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-11-17 16:18:54 +0000

    Initial commit

---
+++ TransactionProcessor.MobileConfiguration/README.md
@@ -0,0 +1,3 @@
+# TransactionProcessor.MobileConfiguration
+
+
---
+++ TransactionProcessor.MobileConfiguration/server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+
+app.get('/configuration/:id',
+    function(req, res)
+    {        
+        var sql = require("mssql");
+
+        // config for your database
+        var config = {
+            user: 'mobileconfig',
+            password: 'Se9RqFy~FE-y',
+            server: 'den1.mssql7.gear.host',
+            database: 'mobileconfig'
+        };
+
+        // connect to your database
+        sql.connect(config, function (err) {
+
+          if (err) console.log(err);
+
+          // create Request object
+          var request = new sql.Request();
+
+            var query =
+            "select top 1 DeviceIdentifier, ClientId, ClientSecret, EstateManagement, TransactionProcessorACL from vwDeviceConfiguration where DeviceIdentifier = '" + req.params.id + "'";
+
+          console.log(query);
+          // query to the database and get the records
+            request.query(query, function (err, recordset)
+          {
+            if (err)
+              console.log(err);
+
+              var record = recordset.recordsets[0][0];
+              console.log(JSON.stringify(record));
+
+            // send records as a response
+            var result = {
+              "deviceIdentifier": record["DeviceIdentifier"],
+              "clientId": record["ClientId"],
+              "clientSecret": record["ClientSecret"],
+              "estateManagement": record["EstateManagement"],
+              "transactionProcessorACL": record["TransactionProcessorACL"],
+            }
+            //console.log(JSON.stringify(result));
+            res.send(JSON.stringify(result));
+          });
+        });
+
+        //res.end(JSON.stringify(config));
+    });
+
+var server = app.listen(80, function ()
+{
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at http://%s:%s", host, port);
+})
